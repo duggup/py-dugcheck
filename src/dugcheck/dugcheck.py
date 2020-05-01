5c91bd13d21a53041e6b3991a334c9d1ba022359
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2020-05-01 11:28:05 +0530

    error handler.

--- src/dugcheck/dugcheck.py
+++ src/dugcheck/dugcheck.py
@@ -1,8 +1,10 @@
 # encoding=utf-8
+import json
 import logging
 import os
+import traceback
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from gevent.pywsgi import WSGIServer
 
@@ -22,20 +24,33 @@ class Dugcheck(object):
         """
         # register a route for question_id and let the frontend hit
         # APIs on this route.
-        self.logger.info(f"registering solution to question: {question_id}")
-
         def foo(fn):
             def handler():
-                inp = {}
-                out = fn(inp)
-                return jsonify(out)
+                inp, out, status_code = {}, {}, 200
 
-            self.app.add_url_rule(f"/{question_id}", f"evaluator_{question_id}", handler)
+                data = request.stream.read()
+                try:
+                    inp = json.loads(data)
+                except json.JSONDecodeError:
+                    pass
+
+                try:
+                    out = fn(inp)
+                except Exception as e:
+                    out = {"error": str(e), "traceback": traceback.format_exc()}
+                    status_code = 500
+
+                return jsonify(out), status_code
+
+            self.app.add_url_rule(
+                f"/{question_id}", f"evaluator_{question_id}", handler, methods=["POST"]
+            )
+            self.logger.info(f"registering solution to question: {question_id}")
             return handler
 
         return foo
 
     def run(self, host="0.0.0.0", port="3002", debug=True):
-        self.logger.info(f"server running at: http://{host}:{port}")
         http_server = WSGIServer((host, port), self.app)
+        self.logger.info(f"server running at: http://{host}:{port}")
         http_server.serve_forever()
